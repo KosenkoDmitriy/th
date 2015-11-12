78969f8fec5c92ea21c48a2c06b7dd41983b5c30
KosenkoDmitriy <kosenkodmitryv@gmail.com>
2015-11-12 14:25:29 +0400

    Added JSIL init project for web platform (because monogame doesn't support it).

---
+++ TH/Assets/Bomb2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb2 : MonoBehaviour {
+    public string bombName;
+    private string privateBombName;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Bomb2 Start !!" + bombName.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Bomb2 update !!");
+    }
+
+    void BombSound() {
+        Debug.Log("Bomb 2 sound !!");
+    }
+}
