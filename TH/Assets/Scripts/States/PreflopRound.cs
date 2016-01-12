58de3e9e73639fbe3a33088f93337ed0ebcd605c
dmitry <kosenkodmitryv@gmail.com>
2016-01-12 08:48:09 +0300

    refactored bet sub rounds in the preflop

--- TH/Assets/Scripts/States/PreflopRound.cs
+++ TH/Assets/Scripts/States/PreflopRound.cs
@@ -36,57 +36,38 @@ public class PreflopRound : BetRound {
 		if (!game.state.isWaiting) {
 			var player = game.playerIterator.NextActive();
 
-			if (player.position == game.playerIterator.LastActive().position) { // last player
-				isCanToRaise = !isCanToRaise;
-				if (!isCanToRaise) {
-					
-					if (IsNextBetRound()) {
-						LastAction(); // next bet round if no any raise
-					}
-
-				}
-			}
-			
-
 			if (player.isReal) {
 				game.state.isWaiting = true;
 				if (isCanToRaise) {
 					game.ui.btnRaise.GetComponent<Button>().interactable = true;
-					//					game.ui.btnCheck.GetComponent<Button>().interactable = false;
 				} else {
 					game.ui.btnRaise.GetComponent<Button>().interactable = false;
-					//					game.ui.btnCheck.GetComponent<Button>().interactable = true;
 				}
 			} else {
 				player.actionFinal = player.GetFinalAction(betMax, isCanToRaise);
+				player.actionTip.Do(game);
+			}
 
-				if (player.betAlreadyInvestedBeforeAction >= betMax) {
-					game.playerIterator = new PlayerIterator(game.playerCollection); // start from first to act player
-					betMax = player.betAlreadyInvestedBeforeAction;
-					subRoundCurrent++;	// next bet sub round
-					// below code already implemented in the FinalAction()
-//					if (subRoundCurrent >= subRoundMaxSize) {
-//						subRoundCurrent = 0;
-//						betMax = 0;
-//						game.state = new FlopRound(game);
-//					}
-				} else {
-					player.actionFinal.Do(game);
-					if (player.betAlreadyInvestedBeforeAction > betMax) {
-						if (isCanToRaise) {
-							betMax = player.betAlreadyInvestedBeforeAction;
-//							isRaised = true;
-						} else {
-							//TODO call
+			if (player.position == game.playerIterator.LastActive().position) { // last player
+				isCanToRaise = false;
+				if (!isCanToRaise) {
+					if (subRoundCount < subRoundMaxSize) {
+						subRoundCount++;
+					} else if (subRoundCount == subRoundMaxSize) {	// last subround
+						if (IsNextBetRound()) {						// no any raise
+							subRoundCount++; // LastAction();		// next bet round
+						} else {			
+							isCanToRaise = false; // repeat last subround with disabled raise action
 						}
+					}
+					if (IsNextBetRound()) {
+						LastAction(); // next bet round if no any raise in any subround
 					}
 				}
 			}
 			game.ui.UpdatePlayer(player);
-
 		}
 //		game.state.isWaiting = true;
-//		isWaiting = true;
 //		game.ui.StartCoroutine (game.ui.UpdatePlayer (player));
 	}
 
