5775fd433c684459f24d23b83bcf3105b45b6b5e
dmitry <kosenkodmitryv@gmail.com>
2016-06-25 17:22:01 +0300

    fixed image size for f.php

--- TH/Builds/f.php
+++ TH/Builds/f.php
@@ -1,5 +1,5 @@
 <?php
-$url = $_GET["url"];
+$url = $_GET["url"] + "&width=" + $_GET["width"] + "&height=" + $_GET["height"];
 if (!preg_match("~^https?://~i", $url)) {
   $url = "http://" . $url;
 }
