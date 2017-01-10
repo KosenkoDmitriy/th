011914670a9e1cf8db2eaa13c2139968101985d3
dmitry <kosenkodmitryv@gmail.com>
2017-01-10 22:34:56 +0300

    change login url, big fw

--- TH/Assets/Scripts/Settings.cs
+++ TH/Assets/Scripts/Settings.cs
@@ -110,11 +110,11 @@ static class Settings
 	public static readonly string actionFbAvatar = "avatar_url";
 	public static string facebookFinalImageUrl = "";
 
-	public static readonly string urlSignUp = host + "/sign_in_up";
+	public static readonly string urlSignUp = host + "/sign_up";
 	public static readonly string urlMobileSignUp = host + "/mobile/signup";
 
 	public static readonly string urlRestore = host + "/restore"; //TODO: implement on website
-    public static readonly string urlLogin = urlSignUp;
+	public static readonly string urlLogin = host + "/sign_in";
     public static readonly string urlBuy = host + "#credits";// "/buy"; //TODO: implement on website
     public static readonly string urlInviteFriend = host + "/invite_friend"; //TODO: implement on website
     public static readonly string urlFortuneWheel = host + "#credits";//"/fortune_wheel"; //TODO: implement on website
